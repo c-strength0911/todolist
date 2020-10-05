7ecec2d40efa871ff66bd2e1fd0c425f9e402027
c-strength0911 <c-strength0911@users.noreply.github.com>
2020-10-05 16:38:19 +0900

    수정

--- todo.js
+++ todo.js
@@ -3,21 +3,21 @@ const todoDelBtn = document.querySelectorAll(".todo_del");
 const todos = document.querySelector(".todos");
 const list = document.querySelectorAll(".todos li");
 const addBtn = document.getElementById("add");
-
+let 
 function inputAction() {
   console.log(addList.value);
   //   여기가 안됨
 }
 
-// function generateTemplate() {
-//   const html = `
-//     <li>
-//     <input type="checkbox" id="todo_${}" />
-//     <label for="todo_${}"><span class="check"></span>${addList.value}</label>
-//     <div class="todo_del"><ion-icon name="trash-outline"></ion-icon></div>
-//     </li>
-//     `;
-//   todos.innerHTML += html;
-// //   미완성
-// }
+function generateTemplate() {
+  const html = `
+    <li>
+    <input type="checkbox" id="todo_${}" />
+    <label for="todo_${}"><span class="check"></span>${addList.value}</label>
+    <div class="todo_del"><ion-icon name="trash-outline"></ion-icon></div>
+    </li>
+    `;
+  todos.innerHTML += html;
+//   미완성
+}
 addBtn.addEventListener("click", inputAction);
