8b05fb97774ed8dcb9f648e60e4a0106f73946cc
c-strength0911 <c-strength0911@users.noreply.github.com>
2020-10-05 10:51:42 +0900

    10/05 js 작성

--- todo.js
+++ todo.js
@@ -1,3 +1,23 @@
-const inputTodoList = document.getElementById("inputTodoList");
-const submit = document.getElementById("submit");
-const todoDelete = document.getElementsByClassName("todo_del");
+const addList = document.getElementById("addList");
+const todoDelBtn = document.querySelectorAll(".todo_del");
+const todos = document.querySelector(".todos");
+const list = document.querySelectorAll(".todos li");
+const addBtn = document.getElementById("add");
+
+function inputAction() {
+  console.log(addList.value);
+  //   여기가 안됨
+}
+
+// function generateTemplate() {
+//   const html = `
+//     <li>
+//     <input type="checkbox" id="todo_${}" />
+//     <label for="todo_${}"><span class="check"></span>${addList.value}</label>
+//     <div class="todo_del"><ion-icon name="trash-outline"></ion-icon></div>
+//     </li>
+//     `;
+//   todos.innerHTML += html;
+// //   미완성
+// }
+addBtn.addEventListener("click", inputAction);
