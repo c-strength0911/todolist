d980131254ad4d067c01292fbb580ff401217345
c-strength0911 <c-strength0911@users.noreply.github.com>
2020-10-06 10:46:01 +0900

    클래스, id 이름 수정 / js makeLineThroght 만드는 중

--- todo.js
+++ todo.js
@@ -1,23 +1,28 @@
-const addList = document.getElementById("addList");
-const todoDelBtn = document.querySelectorAll(".todo_del");
+const todoInput = document.getElementById("todoInput");
+const todoDelBtn = document.querySelectorAll(".todos__del-btn");
 const todos = document.querySelector(".todos");
 const list = document.querySelectorAll(".todos li");
-const addBtn = document.getElementById("add");
-let 
+const insertAddBtn = document.getElementById("insertAddBtn");
+const labels = document.querySelectorAll("label");
+let listLength = list.length;
+
 function inputAction() {
-  console.log(addList.value);
-  //   여기가 안됨
+  console.log(todoInput.value);
+  generateTemplate();
 }
 
 function generateTemplate() {
   const html = `
     <li>
-    <input type="checkbox" id="todo_${}" />
-    <label for="todo_${}"><span class="check"></span>${addList.value}</label>
-    <div class="todo_del"><ion-icon name="trash-outline"></ion-icon></div>
+    <input type="checkbox" id="todo_${listLength}" />
+    <label for="todo_${listLength}"><span class="check"></span>${todoInput.value}</label>
+    <div class="todos__del-btn"><ion-icon name="trash-outline"></ion-icon></div>
     </li>
     `;
   todos.innerHTML += html;
-//   미완성
+  //   미완성
 }
-addBtn.addEventListener("click", inputAction);
+
+function makeLineThroght(e) {}
+todos.addEventListener("click", makeLineThroght(event));
+insertAddBtn.addEventListener("click", inputAction);
